9f6c7bd33ffec51ae3441b60c00ae8b816bf896a
GestaIt <gestaltsemailyes@gmail.com>
2024-02-16 11:53:52 -0500

    Add server url

--- src/services/WhitePebble.ts
+++ src/services/WhitePebble.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {DataResponse} from "@/services/types";
 
-axios.defaults.baseURL = 'http://localhost'
+axios.defaults.baseURL = 'http://208.73.204.74'
 
 export async function getData() {
   return axios.get<DataResponse>('/datadjfddjdjdjdjdjdjdjdjdjdjdjdjdhjdhdjhdjhdjdhudhddh')
